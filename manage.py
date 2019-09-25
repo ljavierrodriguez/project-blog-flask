731913e1a3f2a3045a04801f9be0c026eec9aeb2
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2019-09-25 15:14:21 -0300

    Send Email and Relationships

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, jsonify, request
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import db, User
+from flask_mail import Mail, Message
+from models import db, User, Address
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,7 +13,30 @@ app.config['DEBUG'] = True
 app.config['ENV'] = 'development'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEDIR, 'test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'ljavierrodriguez@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 db.init_app(app)
+
+'''
+MAIL_SERVER : default ‘localhost’
+MAIL_PORT : default 25
+MAIL_USE_TLS : default False
+MAIL_USE_SSL : default False
+MAIL_DEBUG : default app.debug
+MAIL_USERNAME : default None
+MAIL_PASSWORD : default None
+MAIL_DEFAULT_SENDER : default None
+MAIL_MAX_EMAILS : default None
+MAIL_SUPPRESS_SEND : default app.testing
+MAIL_ASCII_ATTACHMENTS : default False
+'''
+
+mail = Mail()
+mail.init_app(app)
 
 Migrate = Migrate(app, db)
 
@@ -55,6 +79,55 @@ def users(id=None):
             db.session.commit()
             return jsonify({"message":"object deleted"}), 200
 
+
+@app.route('/addresses', methods=['GET', 'POST'])
+@app.route('/addresses/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def addresses(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            address = Address.query.get(id)
+            if address:
+                return jsonify(address.serialize()), 200
+            else:
+                return jsonify({"error":"not found"}), 404
+        else:
+            addresses = Address.query.all()
+            json_list=[address.serialize() for address in addresses]
+            return jsonify(json_list), 200
+
+    if request.method == 'POST':
+       address = Address(email=request.json.get('email'), user_id=request.json.get('user_id'))
+       db.session.add(address)
+       db.session.commit()
+       return jsonify(address.serialize()), 201
+
+
+    if request.method == 'PUT':
+        if id is not None:
+            address = Address.query.get(id)
+            address.email = request.json.get('email')
+            address.user_id = request.json.get('user_id')
+            db.session.commit()
+            return jsonify(address.serialize()), 201
+    
+    if request.method == 'DELETE':
+        if id is not None:
+            address = Address.query.get(id)
+            db.session.delete(address)
+            db.session.commit()
+            return jsonify({"message":"object deleted"}), 200
+
+@app.route('/sendmail', methods=['POST'])
+def sendmail():
+    msg = Message('Hello', 
+        sender = 'ljavierrodriguez@gmail.com', 
+        recipients = ['lrodriguez@4geeks.co']
+    )
+    msg.subject = "Esto es una prueba"
+    msg.html = "<h1>Hola Mundo</h1>"
+    mail.send(msg)
+
+    return jsonify({"message":"Email sent"}), 200
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -6,12 +6,35 @@ class User(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-
+    addresses = db.relationship('Address', backref='user', lazy=True)
     def __repr__(self):
         return '<User %r>' % self.name
 
     def serialize(self):
+        json_list=None
+        if self.addresses:
+            json_list=[address.serialize() for address in self.addresses]
+
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "addresses": json_list
+        }
+
+
+class Address(db.Model):
+    __tablename__='address'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+        nullable=False)
+
+    def __repr__(self):
+        return '<Address %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "user": self.user.name
         }
